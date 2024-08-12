3c3d8d5da7ea5950e30117280584900874789cbc
OgliariNatan <natanogliari@gmail.com>
2024-08-12 10:46:50 -0300

    implementação

--- lista_encadeada.py
+++ lista_encadeada.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 
+
+
+
+class ItemLista:
+
+    def __init__(self, data=0, nextItem=None):
+
+        self.data = data
+        self.next = nextItem
+
+ 
+
+    def __repr__(self):
+        return '%s => %s' % (self.data, self.next)
+    
+class ListaEncadeada:
+
+    def __init__(self):
+        self.head = None
+
+ 
+
+    def __repr__(self):
+        return "%s" % (self.head)
