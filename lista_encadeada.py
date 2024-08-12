d2d1bdf753151b71bea3508cec28696542f0aea9
OgliariNatan <natanogliari@gmail.com>
2024-08-12 10:52:24 -0300

    j

--- lista_encadeada.py
+++ lista_encadeada.py
@@ -20,8 +20,21 @@ class ListaEncadeada:
 
     def __init__(self):
         self.head = None
-
  
-
     def __repr__(self):
         return "%s" % (self.head)
+    
+    def insere(self, lista, data):
+
+        # cria um objeto para armazenar um novo item da lista 
+        item = ItemLista(data)
+
+        # o head é apontado como próximo item
+        item.next = lista.head
+
+        # o item atual se torna o head
+        lista.head = item
+
+#minha_lista = ListaEncadeada()
+
+#print(type(minha_lista))
