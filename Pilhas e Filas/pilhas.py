e8eea39fd0ce79735e9a0bb097f8caecea655f34
OgliariNatan <natanogliari@gmail.com>
2024-08-13 09:47:21 -0300

    pilhas

--- Pilhas e Filas/pilhas.py
+++ Pilhas e Filas/pilhas.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+
+pilha = []
+print(f'Minha pilha é:\t{pilha}')
+
+pilha.append('N')
+print(f'Minha pilha é:\t{pilha}')
+
+pilha.append('A')
+print(f'Minha pilha é:\t{pilha}')
+
+pilha.append('T')
+print(f'Minha pilha é:\t{pilha}')
+
+pilha.append('A')
+print(f'Minha pilha é:\t{pilha}')
+
+pilha.append('N')
+print(f'Minha pilha é:\t{pilha}')
+
+print('Removendo elementos\n\n')
+
+elemento_removido = pilha.pop()
+print(f'Elemento removido:\t{elemento_removido}')
+print(f'Minha pilha é:\t{pilha}')
+
+elemento_removido = pilha.pop()
+print(f'Elemento removido:\t{elemento_removido}')
+print(f'Minha pilha é:\t{pilha}')
+
+elemento_removido = pilha.pop()
+print(f'Elemento removido:\t{elemento_removido}')
+print(f'Minha pilha é:\t{pilha}')
+
+elemento_removido = pilha.pop()
+print(f'Elemento removido:\t{elemento_removido}')
+print(f'Minha pilha é:\t{pilha}')
+
+elemento_removido = pilha.pop()
+print(f'Elemento removido:\t{elemento_removido}')
+print(f'Minha pilha é:\t{pilha}')
